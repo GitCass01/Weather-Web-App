d1c7bd5aedc335b61fe883318423b1dc6ea9a11b
GitCass01 <72577093+GitCass01@users.noreply.github.com>
2022-04-23 21:44:52 +0200

    js formatted

--- Project/js/scriptJS.js
+++ Project/js/scriptJS.js
@@ -1,27 +1,27 @@
 // funzione per mostrare la card specifica per la città innserita nella search bar
 function showMe() {
-    var card = document.getElementById('hidden-card');
-    var city = document.getElementById('floatingInput');
+  var card = document.getElementById('hidden-card');
+  var city = document.getElementById('floatingInput');
 
-    if (city.value.trim()) {
-        if (card.style.display == '' || card.style.display == 'none') {
-            card.style.display = 'block';
-            document.getElementById('city').innerHTML = city.value;
-        }
-        else {
-            card.style.display = 'none';
-        }
+  if (city.value.trim()) {
+    if (card.style.display == '' || card.style.display == 'none') {
+      card.style.display = 'block';
+      document.getElementById('city').innerHTML = city.value;
     }
+    else {
+      card.style.display = 'none';
+    }
+  }
 }
 
 // shadow on mouse hover per le card - JQuery
 $(document).ready(function () {
-    $(".card").hover(
-      function () {
-        $(this).addClass("shadow");
-      },
-      function () {
-        $(this).removeClass("shadow");
-      }
-    );
-  });
+  $(".card").hover(
+    function () {
+      $(this).addClass("shadow");
+    },
+    function () {
+      $(this).removeClass("shadow");
+    }
+  );
+});
