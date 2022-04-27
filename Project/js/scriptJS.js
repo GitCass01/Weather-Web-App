7a82133dfd0c33818a8e85a456b60ae2b132b58d
GitCass01 <72577093+GitCass01@users.noreply.github.com>
2022-04-27 12:58:42 +0200

    added current weather js with owp (hidden card)

--- Project/js/scriptJS.js
+++ Project/js/scriptJS.js
@@ -1,13 +1,69 @@
 // funzione per mostrare la card specifica per la città innserita nella search bar
+// + current weather
+// direct geocoding call : http://api.openweathermap.org/geo/1.0/direct?q=city&limit=3&appid=fb1d036e9880437a98ec66f6e4daab01
+// one call api : https://api.openweathermap.org/data/2.5/onecall?lat=xxxx&lon=xxxx&exclude=minutely,hourly,daily&appid=fb1d036e9880437a98ec66f6e4daab01
+// icon url : http://openweathermap.org/img/wn/xxx@2x.png
+const icon_url = "http://openweathermap.org/img/wn/10d@2x.png";
+
 function showMe() {
-  var card = document.getElementById('hidden-card');
-  var city = document.getElementById('floatingInput');
+  let card = document.getElementById('hidden-card');
+  let city = document.getElementById('floatingInput');
 
   if (city.value.trim()) {
-    if (card.style.display == '' || card.style.display == 'none') {
-      card.style.display = 'block';
-      document.getElementById('city').innerHTML = city.value;
-    }
+    card.style.display = 'block';
+    document.getElementById('city').innerHTML = city.value;
+
+    const geo_url = 'http://api.openweathermap.org/geo/1.0/direct?';
+    const params = {
+      q: city.value,
+      limit: 3,
+      appid: 'fb1d036e9880437a98ec66f6e4daab01'
+    };
+    const query = new URLSearchParams(params).toString().replace("%2C", ",");
+    const final_url = geo_url + query;
+
+    fetch(final_url)
+      .then(response => response.json())
+      .then(result => {
+        const lat = result[0].lat;
+        const lon = result[0].lon;
+
+        const onecall_url = 'https://api.openweathermap.org/data/2.5/onecall?';
+        const params = {
+          lat: lat,
+          lon: lon,
+          exclude: 'minutely,hourly,daily',
+          units: 'metric',
+          lang: 'it',
+          appid: 'fb1d036e9880437a98ec66f6e4daab01'
+        }
+        const query_weather = new URLSearchParams(params).toString().replaceAll("%2C", ",");
+        const call = onecall_url + query_weather;
+
+        fetch(call)
+          .then(response => response.json())
+          .then(result => {
+            console.log(result);
+
+            let citycard = document.getElementById('hidden-card-body').children;
+
+            citycard[0].innerHTML = timestampToDate(result.current.dt);
+            citycard[1].src += result.current.weather[0].icon + "@2x.png";
+              citycard[2].innerHTML = result.current.temp + "° " + result.current.weather[0].description;
+            if (result.alerts) {
+              citycard[3].style.display = 'block';
+              citycard[3].innerHTML = result.alerts.event;
+              citycard[3].classList.add("bg-danger");
+            }
+            const lis = citycard[4].getElementsByTagName("li");
+            lis[0].innerHTML += result.current.humidity + "%";
+            lis[1].innerHTML += result.current.pressure + " hPa";
+            lis[2].innerHTML += result.current.wind_speed + " m/s";
+
+          })
+          .catch(err => console.log("err: ", err));
+      })
+      .catch(err => console.log("err: ", err));
   }
 }
 
@@ -23,7 +79,7 @@ $(document).ready(function () {
   );
 });
 
-// geolocalizzazione
+// geolocalizzazione (+ reverse geocoding)
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition, errorFunction);
@@ -53,7 +109,7 @@ function showPosition(position) {
 
       const city_name = result['address']['town'];
       const country = result['address']['country_code'].toUpperCase();
-      
+
       document.getElementById('floatingInput').value = city_name + ", " + country;
     })
     .catch((err) => console.log("err: ", err));
@@ -62,3 +118,21 @@ function showPosition(position) {
 function errorFunction() {
   alert("Geocoder failed");
 }
+
+// conversione da timestamp Unix, UTC in "gg/mm/aaaa"
+function timestampToDate(timestamp) {
+  // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+  var date = new Date(timestamp * 1000);
+
+  return date.toLocaleString('it-IT');
+}
+
+// CURRENT WEATHER (card in homepage: Milano, Londra, Tokyo, New York)
+// direct geocoding call : http://api.openweathermap.org/geo/1.0/direct?q=Milano&limit=3&appid=fb1d036e9880437a98ec66f6e4daab01
+// one call api : https://api.openweathermap.org/data/2.5/onecall?lat=xxxx&lon=xxxx&exclude=minutely,hourly,daily&appid=fb1d036e9880437a98ec66f6e4daab01
+
+
+
+// weekly (7 days) weather
+
+// historical weather (5 days)
